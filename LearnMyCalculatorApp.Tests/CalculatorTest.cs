d409ecc2e70cd3220a6b25ac3f61a5ef88ea3ad9
Jose Leonardo Quiñones <desarrollojlq@outlook.es>
2023-09-27 11:36:31 -0500

    Sharpen your test skills

--- LearnMyCalculatorApp.Tests/CalculatorTest.cs
+++ LearnMyCalculatorApp.Tests/CalculatorTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LearnMyCalculatorApp;
+using FluentAssertions;
 
 namespace LearnMyCalculatorApp.Tests
 {
@@ -11,6 +12,41 @@ namespace LearnMyCalculatorApp.Tests
     [TestClass]
     public class CalculatorTest
     {
+        /// <summary>
+        /// Puede usar pruebas controladas por datos para ejecutar el mismo método de prueba muchas veces con varios parámetros. Esta técnica también se denomina prueba parametrizada. Permite evitar la repetición en el código al mismo tiempo que se comprueba la misma función con un conjunto completo de entradas de datos diferentes.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="expected"></param>
+        [DataTestMethod]
+        [DataRow(1,1,2)]
+        [DataRow(2,2,4)]
+        [DataRow(3,3,6)]
+        [DataRow(0,0,1)]// The test run with this row fails
+        public void AddDataTest(int x, int y, int expected)
+        {
+            var calculator=new Calculator();
+            var actual =calculator.Add(x,y);    
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Las aserciones de Fluent son un conjunto popular de métodos de extensión proporcionados por la comunidad de .NET que pueden ayudarle a identificar claramente los métodos de aserción. Usa lenguaje legible para facilitar la escritura y lectura de pruebas.
+        /// </summary>
+        [TestMethod]
+        public void AddTestFluentassertion() { 
+        
+            var calculator = new Calculator();
+            var actual = calculator.Add(1, 1);
+
+            //Non-fluent asserts:
+            //Assert.AreEqual(actual,2);
+            //Assert.AreNotEqual(actual,1);
+
+            //same asserts as what is commented out above, but using Fluent Assertions
+            actual.Should().Be(2).And.NotBe(1);
+
+        }
         [TestMethod]
         public void CalculatorNullTest()
         {
